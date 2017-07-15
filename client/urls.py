a5939ef7d54aee237b9514a272d66c299fa6876c
Edward Ward <edwardwardward@gmail.com>
2017-07-15 17:34:19 +0100

    add in basic client view

--- client/urls.py
+++ client/urls.py
@@ -3,5 +3,6 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
+    url(r'^(?P<client_id>[0-9]+)/$', views.detail, name='detail'),
     url(r'^$', views.index, name='index')
 ]
--- client/views.py
+++ client/views.py
@@ -5,3 +5,6 @@ from django.http import HttpResponse
 def index(request):
     return HttpResponse("Hello, world. You're at the polls index.")
 # Create your views here.
+
+def detail(request, client_id):
+    return HttpResponse("You're looking at client %s." % client_id)
